0d6c385b820eb51a90dd2bc9dd39e7089c68a4f1
Pedroormaza97 <jrpedro1606@gmail.com>
2021-06-10 12:21:35 -0500

    Plantilla limpia

---
+++ Vistas/plantilla.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<link rel="icon" href="vistas/img/plantilla/logomini.png" type="image/ico" />
+
+    <title>Sistema Médico | </title>
+    <!--*********************************************
+    *                     Plugins CSS                *
+    *************************************************-->
+    <!-- Bootstrap -->
+    <link href="vistas/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="vistas/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="vistas/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="vistas/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+
+    <!-- bootstrap-progressbar -->
+    <link href="vistas/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="vistas/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+    <!-- bootstrap-daterangepicker -->
+    <link href="vistas/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <!-- Custom Theme Style -->
+    <link href="vistas/build/css/custom.css" rel="stylesheet">
+  </head>
+
+
+    <!--*********************************************
+    *                     CUERPO DEL CODIGO         *
+    *************************************************-->
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+
+
+   <?php
+      include "vistas/modulos/cabezote.php";
+      //include "vistas/modulos/menulateral.php";
+      include "vistas/modulos/contenido.php";
+      include "vistas/modulos/footer.php";
+
+    ?>
+
+
+    <!--*********************************************
+    *                     Plugins CSS                *
+    *************************************************-->
+    <!-- jQuery -->
+    <script src="vistas/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="vistas/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- FastClick -->
+    <script src="vistas/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="vistas/vendors/nprogress/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="vistas/vendors/Chart.js/dist/Chart.min.js"></script>
+    <!-- gauge.js -->
+    <script src="vistas/vendors/gauge.js/dist/gauge.min.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="vistas/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <!-- iCheck -->
+    <script src="vistas/vendors/iCheck/icheck.min.js"></script>
+    <!-- Skycons -->
+    <script src="vistas/vendors/skycons/skycons.js"></script>
+    <!-- Flot -->
+    <script src="vistas/vendors/Flot/jquery.flot.js"></script>
+    <script src="vistas/vendors/Flot/jquery.flot.pie.js"></script>
+    <script src="vistas/vendors/Flot/jquery.flot.time.js"></script>
+    <script src="vistas/vendors/Flot/jquery.flot.stack.js"></script>
+    <script src="vistas/vendors/Flot/jquery.flot.resize.js"></script>
+    <!-- Flot plugins -->
+    <script src="vistas/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+    <script src="vistas/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+    <script src="vistas/vendors/flot.curvedlines/curvedLines.js"></script>
+    <!-- DateJS -->
+    <script src="vistas/vendors/DateJS/build/date.js"></script>
+    <!-- JQVMap -->
+    <script src="vistas/vendors/jqvmap/dist/jquery.vmap.js"></script>
+    <script src="vistas/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+    <script src="vistas/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="vistas/vendors/moment/min/moment.min.js"></script>
+    <script src="vistas/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="vistas/build/js/custom.min.js"></script>
+        
+      </div>
+    </div>
+  </body>
+</html>
