24a2d7aa456403d28bf36c0df34fc764025ab537
Damianstalck <63911045+Damianstalck@users.noreply.github.com>
2021-11-24 20:56:41 -0500

    Delete controlador_listar_Parroquia.php

--- controlador_listar_Parroquia.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "../Modelos/lugares.modelo.php";
-$MU = new mdl_lugares();
-$idCanton = $_POST['idCanton'];
-$consulta = $MU->listar_parroquia($idCanton);
-echo json_encode($consulta);
-
-?>
