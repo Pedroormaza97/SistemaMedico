055a36d57893ff14dc5fe1f5bbfebb1e8b1d4957
Damianstalck <63911045+Damianstalck@users.noreply.github.com>
2021-11-24 20:56:28 -0500

    Delete controlador_listar_Canton.php

--- controlador_listar_Canton.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "../Modelos/lugares.modelo.php";
-$MU = new mdl_lugares();
-$idprovincia = $_POST['idprovincia'];
-$consulta = $MU->listar_canton($idprovincia);
-echo json_encode($consulta);
-
-?>
